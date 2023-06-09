f64bb2628ddbf3c70087c271cc3fcea2108aab12
root <root@LAPTOP-CLJLRK5D>
2023-06-09 16:04:30 -0300

    fixed joinS in ArrSeq. Commented code.

--- ArrSeq.hs
+++ ArrSeq.hs
@@ -4,46 +4,56 @@ import Seq
 import Par
 import qualified Arr as A
 
+-- Funcion auxiliar para reduceS y scanS
+-- Contrae una secuencia aplicando el operador recibido de par en par sobre los 
+-- elementos de la secuencia. Si la cantidad de elementos es impar, deja el ultimo
+-- sin operar.
 contract :: (a -> a -> a) -> A.Arr a -> A.Arr a
 contract f xs = tabulateS g n'
               where
-                n = lengthS xs
-                k = n `div` 2
-                n' = if even n then k else k + 1
-                g i = if 2 * i + 1 /= n then f (xs A.! (2*i)) (xs A.! (2*i + 1))
-                                        else (xs A.! (2*i))
+                n = lengthS xs 
+                k = n `div` 2 
+                n' = if even n then k else k + 1 -- se precalcula la longitud de la siguiente contracción de la secuencia
+                g i = if 2 * i + 1 /= n then f (xs A.! (2*i)) (xs A.! (2*i + 1)) -- Si hay al menos 2 elementos, se operan
+                                        else (xs A.! (2*i)) -- Si es el último elemento, se copia a la contracción.
 
+-- Funcion auxiliar para scanS
+-- Expande una secuencia que fue comprimida y reducida previamente
+-- Los elementos que se encuentran en posicion par se mantienen como eran en 
+-- la secuencia recibida. 
+-- Los elementos de posiciones impares i son los resultados de operar el elemento anterior
+-- con el elemento de la posicion 2*i - 1 de la secuencia original recibida.
 expand :: (a -> a -> a) -> A.Arr a -> A.Arr a -> A.Arr a
-expand f ps s = tabulateS g (lengthS s)
+expand f ps s = tabulateS g (lengthS s) -- Se aplica una funcion g que segun el indice realiza distintos calculos
                 where
-                  g i | even i = ps A.! (div i 2)
-                      | otherwise = let (x, y) = ps A.! (div i 2) |||
-                                                 s  A.! (i - 1)
-                                    in f x y
+                  g i | even i = ps A.! (div i 2) -- si esta en una posicion par, mantiene el valor
+                      | otherwise = let (x, y) = (ps A.! (div i 2)) (s  A.! (i - 1)) -- Si esta en una posicion impar    
+                                    in f x y                                         -- realiza la operción necesaria para obtener el valor
+                                                                                     
 
 instance Seq A.Arr where
-  emptyS = A.empty
+  emptyS = A.empty -- Implementación de la secuencia vacía con arreglos
 
-  singletonS x = Seq.fromList [x]
+  singletonS x = Seq.fromList [x] -- Implementación de la secuencia de un solo elemento con arreglos
 
-  lengthS = A.length
+  lengthS = A.length -- Implementación de la función lenghtS con arreglos
 
-  nthS = (A.!)
+  nthS = (A.!) -- Implementación de la indexación de secuencias con arreglos
 
-  tabulateS = A.tabulate
+  tabulateS = A.tabulate -- Implementación de la funcion tabulateS con arreglos
 
-  mapS f xs = tabulateS (f . (nthS xs)) (lengthS xs)
+  mapS f xs = tabulateS (f . (nthS xs)) (lengthS xs) -- Implementación de la función mapS con arreglos
 
-  filterS p xs = joinS $ tabulateS f (lengthS xs) 
+  filterS p xs = joinS $ tabulateS f (lengthS xs)  -- Implementación de la función filterS con arreglos
                 where f i = let x = nthS xs i
                             in if p x then singletonS x 
                                       else emptyS
   
-  appendS xs ys = joinS $ fromList [xs, ys]
+  appendS xs ys = joinS $ fromList [xs, ys] -- Implementación de la concatenación de secuencias con arreglos
 
-  takeS xs n = A.subArray 0 n xs
+  takeS xs n = A.subArray 0 n xs -- Implementación de la función takeS con arreglos
 
-  dropS xs n = A.subArray n ((lengthS xs) - n) xs
+  dropS xs n = A.subArray n ((lengthS xs) - n) xs -- Implementación de la funcion dropS con arreglos
 
   showtS xs | n == 0 = EMPTY
             | n == 1 = ELT (nthS xs 0)
@@ -56,24 +66,18 @@ instance Seq A.Arr where
             | otherwise = CONS (nthS xs 0) (dropS xs 1)
             where n = lengthS xs
 
-  joinS xss | n == 0 = emptyS
-            | n == 1 = nthS xss 0
-            | otherwise = let (l, r) =  joinS (takeS xss m)
-                                        |||
-                                        joinS (dropS xss m)
-                          in appendS l r
-              where
-                n = lengthS xss
-                m = div n 2
+  joinS = A.flatten -- Implementación de la función joinS con arreglos
 
-  reduceS f e xs | n == 0 = e
-                 | n == 1 = f e $ nthS xs 0
-                 | n > 1 = reduceS f e $ contract f xs
-                  where n = lengthS xs
+  -- Implementación de la función reduceS con arreglos 
+  reduceS f e xs | n == 0 = e -- reducción de una secuencia vacía, devuelve el elemento 'e'
+                 | n == 1 = f e $ nthS xs 0 -- reduccion de una secuencia de un solo elemento
+                 | n > 1 = reduceS f e $ contract f xs -- reducción de una secuencia con múltiples elementos,
+                  where n = lengthS xs                 -- se aplica la reducción sobre una contracción de la secuencia.
   
-  scanS f e xs | n == 0 = (emptyS, e)
-               | n == 1 = let x = nthS xs 0 in (singletonS e, f e x)
-               | otherwise = let (s', t) = scanS f e $ contract f xs in (expand f s' xs, t)
-              where n = lengthS xs
-
+  -- Implementación de la función scanS con arreglos
+  scanS f e xs | n == 0 = (emptyS, e) -- scanS de una secuencia vacía
+               | n == 1 = let x = nthS xs 0 in (singletonS e, f e x) -- scanS de una secuencia con un solo elemento
+               | otherwise = let (s', t) = scanS f e $ contract f xs in (expand f s' xs, t) -- scanS de una secuencia con
+              where n = lengthS xs                                                          -- múltiples elementos.
+                                                                                       
   fromList = A.fromList
--- ListSeq.hs
+++ ListSeq.hs
@@ -3,41 +3,52 @@ module ListSeq where
 import Par
 import Seq
 
+-- Funcion auxiliar para reduceS y scanS
+-- Contrae una secuencia aplicando el operador recibido de par en par sobre los 
+-- elementos de la secuencia. Si la cantidad de elementos es impar, deja el ultimo
+-- sin operar.
 contract :: (a -> a -> a) -> [a] -> [a]
-contract _ [] = []
-contract _ [x] = [x]
-contract f (x:y:s) = let (z, zs) = (f x y) ||| contract f s in (z:zs)
-
+contract _ [] = [] -- Contraccion de la secuencia vacia
+contract _ [x] = [x] -- Contraccion de la secuencia de un solo elemento
+contract f (x:y:s) = let (z, zs) = (f x y) ||| contract f s in (z:zs) -- Paralelizacion implementada para la optimizacion 
+                                                                      -- de la profundidad de la funcion
+-- Funcion auxiliar para scanS
+-- Expande una secuencia que fue comprimida y reducida previamente
+-- Los elementos que se encuentran en posicion par se mantienen como eran en 
+-- la secuencia recibida. 
+-- Los elementos de posiciones impares i son los resultados de operar el elemento anterior
+-- con el elemento de la posicion 2*i - 1 de la secuencia original recibida.
 expand :: (a -> a -> a) -> [a] -> [a] -> [a]
 expand f ps s = expand' f ps s
               where
-                expand' f rs [] = rs
-                expand' f rs [x] = rs
-                expand' f (r:rs) (s1:s2:ss) = let (r', rs') = (f r s1) |||
-                                                              expand' f rs ss
-                                              in r:r':rs'
+                expand' f rs [] = rs -- Expansión de la secuencia vacia 
+                expand' f rs [x] = rs -- Expansión de la secuencia de un solo elemento
+                expand' f (r:rs) (s1:s2:ss) = let (r', rs') = (f r s1) |||    -- Se computa el caso de la posicion par e impar
+                                                              expand' f rs ss -- dentro del mismo caso. Paralelizacion agregada
+                                              in r:r':rs'                     -- para optimizacion de la profundidad
 
 instance Seq [] where
-  emptyS = []
+  emptyS = []  -- Implementación de la secuencia vacía con listas
 
-  singletonS x = [x]
+  singletonS x = [x] -- Implementación de la secuencia de un solo elemento con listas
 
-  lengthS = length
+  lengthS = length -- Implementación de la funcion enghtS para secuencias con listas
 
-  nthS xs i = xs !! i
+  nthS xs i = xs !! i -- Implementación de la indexacion en secuencias con listas
 
-  tabulateS f n = mapS f [0..(n-1)]
+  tabulateS f n = mapS f [0..(n-1)]  -- Implementación de la funcion tabulateS con listas
 
+  -- Implementación de la funcion mapS con listas
   mapS f [] = []
-  mapS f (x:xs) = let (y,ys) = f x ||| mapS f xs in y:ys
+  mapS f (x:xs) = let (y,ys) = f x ||| mapS f xs in y:ys -- Implementación de paralelizacion necesaria para
+                                                         -- una optimizacion de la profundidad de la funcion.
+  filterS = filter -- Implementación de la funcion filterS con listas
 
-  filterS = filter
+  appendS = (++) -- Implementación de la concatenacion de secuencias con listas
 
-  appendS = (++)
+  takeS s n = take n s -- Implementación de la función takeS con listas
 
-  takeS s n = take n s
-
-  dropS s n = drop n s
+  dropS s n = drop n s -- Implementación de la función dropS con listas
 
   showtS [] = EMPTY
   showtS [x] = ELT x
@@ -46,14 +57,19 @@ instance Seq [] where
   showlS [] = NIL
   showlS (x:xs) = CONS x xs
 
-  joinS = foldr (++) []
+  joinS = foldr (++) [] -- Implementación de joinS con listas
 
-  reduceS f e []  = e
-  reduceS f e [x] = f e x
-  reduceS f e xs  = let xs' = contract f xs in reduceS f e xs'
-
-  scanS f e []  = (emptyS, e)
-  scanS f e [x] = (singletonS e, f e x)
-  scanS f e xs  = let (s',r) = scanS f e $ contract f xs in (expand f s' xs, r)
+  -- Implementación de la función reduceS con listas
+  reduceS f e []  = e -- reducción de una secuencia vacia, devuelve el elemento recibido 'e'
+  reduceS f e [x] = f e x -- reducción de una secuencia de un solo elemento, opera el elemento 'e' con el de la secuencia
+  reduceS f e xs  = let xs' = contract f xs in reduceS f e xs' -- reducción para una secuencia de múltiples elementos, 
+                                                               -- se aplica la funcion reduceS sobre una contracción de la secuencia
+                                                               -- original con respecto al operador recibido.
+  -- Implementación de la función scanS
+  scanS f e []  = (emptyS, e) -- scanS de la secuencia vacia
+  scanS f e [x] = (singletonS e, f e x) --scanS de la secuencia de un solo elemento
+  scanS f e xs  = let (s',r) = scanS f e $ contract f xs in (expand f s' xs, r) -- scanS de la secuencia de múltiples elementos
+                                                                                -- Se contrae la secuencia antes de ser operada y luego
+                                                                                -- se expande para conseguir el resultado final.
 
   fromList = id
